0644bf5cc8e4d815a641ddd5daf1811b7af4d1a7
Travis Haynes <travis.j.haynes@gmail.com>
2011-12-23 13:14:01 -0800

    added about content; using 'swing' easing and 'slow' duration

--- javascripts/docs.js
+++ javascripts/docs.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   
   if(!window.slider){
-    window.slider = new PageSlider();
+    window.slider = new PageSlider({
+      easing  : 'swing',
+      duration: 'slow'
+    });
     slider.bindTo("a[data-remote=true]", "click");
   }
   
