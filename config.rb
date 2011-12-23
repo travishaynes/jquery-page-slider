267521c4c487bd2da2a833f723128b3b666aa744
Travis Haynes <travis.j.haynes@gmail.com>
2011-12-22 17:11:44 -0800

    initial commit

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Compass configuration
+
+http_path         = "/"
+css_dir           = "stylesheets"
+sass_dir          = "sass"
+images_dir        = "images"
+javascripts_dir   = "javascripts"
+preferred_syntax  = :sass
